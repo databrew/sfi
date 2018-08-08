35d6c0efd6880ce8f0227e389716c84f618a546d
Ben Brew <benmbrew@gmail.com>
2018-08-08 18:25:00 -0400

    working on bubble charts

--- R/sfi_plot_alexander_7.R
+++ R/sfi_plot_alexander_7.R
@@ -6,17 +6,17 @@
 #' @import ggplot2
 #' @import dplyr
 
-sfi_plot_alexander_7 <- function(){
-  
-  # Get data
-  data <- all_data$alexander$f7
-  data$value <- data$value * 100
-  
-  # need to find plots that represent simple pie chart data 
-  
-  # pie chart 
-  
-  # side bar chart
-  
-  # return(list(g1, g2, g3, g4, g5))
-}
+# sfi_plot_alexander_7 <- function(){
+#   
+#   # # Get data
+#   # data <- all_data$alexander$f7
+#   # data$value <- data$value * 100
+#   
+#   # need to find plots that represent simple pie chart data 
+#   
+#   # pie chart 
+#   
+#   # side bar chart
+#   
+#   # return(list(g1, g2, g3, g4, g5))
+# }
