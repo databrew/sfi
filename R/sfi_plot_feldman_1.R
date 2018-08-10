efaf2019517b719400a6b2e704906425d562dbee
Ben Brew <benmbrew@gmail.com>
2018-08-10 04:13:49 -0400

    working on feldman figure 1

--- R/sfi_plot_feldman_1.R
+++ R/sfi_plot_feldman_1.R
@@ -17,7 +17,8 @@ sfi_plot_feldman_1 <- function() {
   data$federal <- ifelse(data$federal == '1', 'Federal', 'State')
   
   # histogram with density
-  g1 <-  ggplot(data, 
+  #g1 <-  
+    ggplot(data, 
                 aes(x = clarity_score, 
                     fill = federal))  +
     geom_histogram(aes(y=..density..), 
@@ -32,50 +33,51 @@ sfi_plot_feldman_1 <- function() {
                                            categorical = TRUE, 
                                            bw = TRUE))
   
-  
-  # jitter
-  g2 <- ggplot(data, 
-               aes(x=federal,
-                   y=clarity_score)) + 
-    geom_jitter(width = 0.3,
-                alpha = 0.1,
-                size = 1) + 
-    labs(x = '',
-         y = 'Clarity Score',
-         title = 'Version 2') + 
-    theme_sfi()
-  
-  # density
-  g3 <- ggplot(data, 
-               aes(x=clarity_score, 
-                   fill = federal)) + 
-    geom_density(alpha = 0.5) + 
-    labs(x = 'Clarity Score',
-         y = 'Density',
-         title = 'Version 3') + 
-    theme_sfi() +
-    scale_fill_manual(name = '',
-                      values = make_colors(length(unique(data$federal)),
-                                           categorical = TRUE, 
-                                           bw = TRUE))
-  
   # violin plot
-  g4 <- ggplot(data, 
+  #g4 <- 
+    ggplot(data, 
                aes(x=federal,
                    y=clarity_score)) + 
-    geom_jitter(width = 0.4, 
-                alpha = 0.2,
-                size = 1) +
-    geom_violin(alpha = 0.5, fill = 'darkgrey') +
+    geom_violin(alpha = 0.5, 
+                fill = 'darkgrey') +
+      geom_jitter(width = 0.4, 
+                  alpha = 0.3,
+                  size = 0.7,
+                  color = '#464646', 
+                  stroke = 0
+                  ) +
     labs(x = '',
          y = 'Clarity Score',
-         title = 'Version 4') + 
+         title = 'Version 4',
+         caption = 'violin plot') + 
     theme_sfi() +
     scale_fill_manual(name = '',
                       values = make_colors(length(unique(data$federal)),
                                            categorical = TRUE, 
                                            bw = TRUE))
   
+    #g5 <- 
+    ggplot(data, 
+           aes(x=federal,
+               y=clarity_score)) + 
+      geom_boxplot(alpha = 0.5, 
+                  fill = 'darkgrey') +
+      geom_jitter(width = 0.4, 
+                  alpha = 0.1,
+                  size = 1,
+                  color = 'black', 
+                  stroke = 0
+      ) +
+      labs(x = '',
+           y = 'Clarity Score',
+           title = 'Version 4',
+           caption = 'violin plot') + 
+      theme_sfi() +
+      scale_fill_manual(name = '',
+                        values = make_colors(length(unique(data$federal)),
+                                             categorical = TRUE, 
+                                             bw = TRUE))
+    
   
   
   out <- list(g1, g2, g3, g4)
