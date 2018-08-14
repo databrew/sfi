6479def92f083cfed8db274ff21b6c7e0113cb02
Ben Brew <benmbrew@gmail.com>
2018-08-14 19:51:52 -0400

    finished with laqueur figure 1

--- R/sfi_plot_laqueur_1.R
+++ R/sfi_plot_laqueur_1.R
@@ -14,101 +14,140 @@ sfi_plot_laqueur_1 <- function(){
   data <- all_data$laqueur$f1
   
   # version 1 
-  g1 <- ggplot(data, 
-               aes(x = year, 
-                   y = number_of_grants)) +
-    geom_line(size = 1, 
-              alpha = 0.6) +
+  g1 <- 
+    ggplot(data, 
+           aes(x = year, 
+               y = number_of_grants)) +
+    geom_point(size = 2, 
+               alpha = 0.9,
+               color = 'black',
+               stroke = 1.5,
+               pch = 1) +
+    geom_line(size = 1,
+              color = '#4D4D4D') +
     labs(x = 'Year',
          y = 'Hearings resulting in grant',
          title = 'Version 1') +
     theme_sfi()
   
-  # version 2 
-  g2 <- ggplot(data, 
-               aes(x = year, 
-                   y = number_of_grants)) +
+  # version 2
+  g2 <- 
+    ggplot(data, 
+           aes(x = year, 
+               y = number_of_grants)) +
+    geom_smooth(method = 'loess',
+                alpha = 0.7,
+                color = 'black',
+                linetype = 0) + 
     geom_point(size = 2, 
-               alpha = 0.6) +
+               color = 'black',
+               fill = 'black',
+               alpha = 0.8,
+               pch = 21) +
     labs(x = 'Year',
          y = 'Hearings resulting in grant',
-         title = 'Version 2') +
+         title = 'Version 2',
+         caption = paste0('Fitted line smoothed with a local regression', '\n', 
+                          'with bands representing standard errors')) +
     theme_sfi()
   
-  # version 3 
-  g3 <- ggplot(data, 
-               aes(x = year, 
-                   y = number_of_grants)) +
+  # version 3
+  g3 <- 
+    ggplot(data, 
+           aes(x = year, 
+               y = number_of_grants)) +
+    geom_smooth(method = 'loess',
+                alpha = 0.5,
+                color = 'black',
+                linetype = 0) + 
     geom_point(size = 3, 
-               alpha = 0.6) +
-    geom_line(size = 1, 
-              alpha = 0.6) +    
+               color = 'black',
+               fill = 'black',
+               alpha = 0.8,
+               pch = 21) +
     labs(x = 'Year',
          y = 'Hearings resulting in grant',
-         title = 'Version 3') +
+         title = 'Version 3',
+         caption = paste0('Fitted line smoothed with a local regression', '\n', 
+                          'with bands representing standard errors')) +
     theme_sfi()
+  
   
   # version 4
-  g4 <- ggplot(data, 
-               aes(x = year, 
-                   y = number_of_grants)) +
-    geom_smooth(method = 'lm',
-                size = 0.5, 
-                alpha = 0.6,
+  g4 <- 
+    ggplot(data, 
+           aes(x = year, 
+               y = number_of_grants)) +
+    geom_smooth(method = 'loess',
+                alpha = 0.7,
                 color = 'black',
-                linetype = 2) +    
-    geom_point(size = 3, 
-               alpha = 0.6) +
+                linetype = 0) + 
+    geom_point(size = 4, 
+               color = 'grey',
+               fill = 'black',
+               alpha = 0.8,
+               pch = 21) +
     labs(x = 'Year',
          y = 'Hearings resulting in grant',
          title = 'Version 4',
-         caption = 'Fitted line estimated with a linear regression') +
+         caption = paste0('Fitted line smoothed with a local regression', '\n', 
+                          'with bands representing standard errors')) +
     theme_sfi()
   
-  # version 4
-  g5 <- ggplot(data, 
-               aes(x = year, 
-                   y = number_of_grants)) +
+  
+  
+  # version 5
+  g5 <- 
+    ggplot(data, 
+           aes(x = year, 
+               y = number_of_grants)) +
     geom_smooth(method = 'loess',
-                size = 0.5, 
+                fill = 'black',
                 alpha = 0.6,
-                color = 'black',
-                linetype = 2) + 
+                linetype = 0,
+                fullrange = TRUE) + 
     geom_point(size = 3, 
-               alpha = 0.6) +
+               color = 'black',
+               fill = '#B2B2B2',
+               alpha = 0.8,
+               pch = 21) +
     labs(x = 'Year',
          y = 'Hearings resulting in grant',
          title = 'Version 5',
-         caption = 'Fitted line smoothed with a local regression') +
-    theme_sfi()
+         caption = paste0('Fitted line smoothed with a local regression', '\n', 
+                          'with bands representing standard errors')) +
+    theme_sfi(bc = 'white')
   
   
   # version 6
-  g6 <- ggplot(data, 
-               aes(x = year, 
-                   y = number_of_grants)) +
-    geom_bar(stat = 'identity', 
-             alpha = 0.8,
-             fill = 'white',
-             color = 'black') +
-    geom_text(aes(label = number_of_grants),
-              size = 1.8, 
-              color = 'black',
-              nudge_y = 25) +
+  g6 <- 
+    ggplot(data, 
+           aes(x = year, 
+               y = number_of_grants)) +
+    geom_smooth(method = 'loess',
+                alpha = 0.6,
+                fill = 'black',
+                linetype = 0) + 
+    geom_point(size = 3, 
+               colour = 'white',
+               alpha = 1,
+               pch = 1) +
     labs(x = 'Year',
          y = 'Hearings resulting in grant',
-         title = 'Version 6') +
-    theme_sfi()
+         title = 'Version 6',
+         caption = 'Fitted line smoothed with a local regression') +
+    theme_sfi(bc = 'grey')
   
   
   # version 7
-  g7 <- ggplot(data, 
-               aes(x = year, 
-                   y = number_of_grants)) +
+  g7 <- 
+    ggplot(data, 
+           aes(x = year, 
+               y = number_of_grants)) +
     geom_bar(stat = 'identity', 
              alpha = 0.8,
-             fill = 'white',
-             color = 'black') +
+             fill = 'black',
+             color = 'grey') +
     geom_text(aes(label = number_of_grants),
               size = 1.8, 
               color = 'black',
@@ -119,52 +158,9 @@ sfi_plot_laqueur_1 <- function(){
     theme_sfi()
   
   
-  # version 8
-  g8 <- ggplot(data, 
-               aes(x = year, 
-                   y = number_of_grants)) +
-    geom_bar(stat = 'identity', alpha = 0.8) +
-    geom_text(aes(label = number_of_grants),
-              size = 1.5, 
-              alpha = 0.6,
-              nudge_y = 25) +
-    labs(x = 'Year',
-         y = 'Hearings resulting in grant',
-         title = 'Version 8') +
-    theme_sfi()
-  
-  
-  # version 9
-  g9 <- ggplot(data, 
-               aes(x = year, 
-                   y = number_of_grants)) +
-    geom_bar(stat = 'identity', alpha = 0.6) +
-    geom_smooth(size = 0.5, color = 'black', se = F) +
-    labs(x = 'Year',
-         y = 'Hearings resulting in grant',
-         title = 'Version 9') +
-    theme_sfi()
-  
-  # version 10
-  g10 <- ggplot(data, 
-                aes(x = year, 
-                    y = number_of_grants)) +
-    geom_point(alpha = 0.1,
-               size = 1)+
-    geom_area(stat = 'identity', 
-              alpha = 0.9,
-              color = 'grey') +
-    labs(x = 'Year',
-         y = 'Hearings resulting in grant',
-         title = 'Version 10') +
-    theme_sfi()
-  
-  
-  
   out <- list(g1, g2, g3, 
               g4, g5, g6, 
-              g7, g8, g9,
-              g10)
+              g7)
   
   return(out)
 }
