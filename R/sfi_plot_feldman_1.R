5e01ffd890331e35c85af7d301a651746dc62753
Ben Brew <benmbrew@gmail.com>
2018-08-17 15:06:05 -0400

    adding feldman, accidentally on master, will change branch

--- R/sfi_plot_feldman_1.R
+++ R/sfi_plot_feldman_1.R
@@ -17,95 +17,29 @@ sfi_plot_feldman_1 <- function() {
   data$federal <- ifelse(data$federal == '1', 'Federal', 'State')
 
   
-  # violin plot
+  
   g1 <- 
     ggplot(data, 
-               aes(x=federal,
-                   y=clarity_score)) + 
-    geom_violin(alpha = 0.5, 
-                fill = 'darkgrey') +
-      geom_jitter(width = 0.4, 
-                  alpha = 0.3,
-                  size = 0.7,
-                  color = '#464646', 
-                  stroke = 0
-                  ) +
+           aes(x=federal,
+               y=clarity_score)) + 
+    
+    geom_violin(alpha = 0.8, 
+                fill = 'black') +
+    geom_jitter(width = 0.3, 
+                alpha = 0.1,
+                color = 'black',
+                size = 1.5,
+                stroke = 1,
+                pch = 1) +
     labs(x = '',
          y = 'Clarity Score',
-         title = 'Version 4',
-         caption = 'violin plot') + 
+         title = 'Version 1') + 
     theme_sfi() +
     scale_fill_manual(name = '',
                       values = make_colors(length(unique(data$federal)),
                                            categorical = TRUE, 
                                            bw = TRUE))
   
-    g2 <- 
-    ggplot(data, 
-           aes(x=federal,
-               y=clarity_score)) + 
-      geom_boxplot(alpha = 0.5, 
-                  fill = 'darkgrey') +
-      geom_jitter(width = 0.4, 
-                  alpha = 0.1,
-                  size = 1,
-                  color = 'black', 
-                  stroke = 0
-      ) +
-      labs(x = '',
-           y = 'Clarity Score',
-           title = 'Version 4',
-           caption = 'violin plot') + 
-      theme_sfi() +
-      scale_fill_manual(name = '',
-                        values = make_colors(length(unique(data$federal)),
-                                             categorical = TRUE, 
-                                             bw = TRUE))
-    
-    g3 <- 
-    ggplot(data, 
-           aes(x=federal,
-               y=clarity_score)) + 
-      geom_violin(alpha = 0.5, 
-                  fill = 'grey',
-                  width = 0.8) +
-      geom_boxplot(alpha = 0.5, 
-                   fill = 'grey',
-                   width = 0.2) +
-      geom_jitter(width = 0.4, 
-                  alpha = 0.1,
-                  size = 1,
-                  color = 'black', 
-                  stroke = 0) +
-      labs(x = '',
-           y = 'Clarity Score',
-           title = 'Version 4',
-           caption = 'violin plot') + 
-      theme_sfi() +
-      scale_fill_manual(name = '',
-                        values = make_colors(length(unique(data$federal)),
-                                             categorical = TRUE, 
-                                             bw = TRUE))
-    
-    
-    # histogram with density
-    g4 <-  
-      ggplot(data, 
-             aes(x = clarity_score, 
-                 fill = federal))  +
-      geom_histogram(aes(y=..density..), 
-                     bins = 50,
-                     alpha = 0.6) +
-      labs(x = 'Clarity score', 
-           y = 'Density', 
-           title = 'Version 1') + 
-      theme_sfi() + 
-      scale_fill_manual(name = '',
-                        values = make_colors(length(unique(data$federal)),
-                                             categorical = TRUE, 
-                                             bw = TRUE))
-  
-    
     
   out <- list(g1, g2, g3, g4)
   return(out)
--- R/sfi_plot_feldman_2.R
+++ R/sfi_plot_feldman_2.R
@@ -19,140 +19,30 @@ sfi_plot_feldman_2 <- function(){
   data$federal <- ifelse(data$federal == '1', 'Federal', 'State')
 
   
-  # boxplot
+  
   g1 <- 
-    ggplot(data, 
-               aes(x=federal,
-                   y=words,
-                   fill = federal)) + 
-    geom_boxplot(alpha = 0.8, 
-                 width = 0.7,
-                 fill = 'black') + 
-      geom_jitter(size = 0.8, 
-                  alpha = 0.1,
-                  pch = 1) +
-    labs(x = '',
-         y = 'Words',
-         title = 'Version 1') + 
-    theme_sfi() + 
-    scale_fill_manual(name = '',
-                      values = make_colors(length(unique(data$federal)),
-                                           categorical = TRUE, 
-                                           bw = TRUE)) + guides(fill = FALSE)
-    
-    # boxplot
-   g2 <- 
-    ggplot(data, 
-           aes(x=federal,
-               y=words,
-               fill = federal)) + 
-      geom_boxplot(alpha = 0.8, 
-                   width = 0.7,
-                   fill = 'darkgrey') + 
-      geom_jitter(size = 1, 
-                  alpha = 0.1,
-                  pch = 22) +
-      labs(x = '',
-           y = 'Words',
-           title = 'Version 2') + 
-      theme_sfi() + 
-      scale_fill_manual(name = '',
-                        values = make_colors(length(unique(data$federal)),
-                                             categorical = TRUE, 
-                                             bw = TRUE)) + guides(fill = FALSE)
- 
-  # jitter with no legend for color difference
-  g3<- 
-    ggplot(data, 
-               aes(x=federal,
-                   y=words)) + 
-    geom_jitter(width = 0.3,
-                alpha = 0.6,
-                size = 3,
-                pch = 44) + 
-    labs(x = '',
-         y = 'Words',
-         title = 'Version 3') + 
-    theme_sfi()
-  
-
-  
-  # violin plot
-   g4 <- 
-     ggplot(data, 
-               aes(x=federal,
-                   y=words)) + 
-
-    geom_violin(alpha = 0.5, 
-                fill = 'grey') +
-       geom_jitter(width = 0.4, 
-                   alpha = 0.1,
-                   color = 'black',
-                   size = 1) +
-    labs(x = '',
-         y = 'Words',
-         title = 'Version 4') + 
-    theme_sfi() +
-    scale_fill_manual(name = '',
-                      values = make_colors(length(unique(data$federal)),
-                                           categorical = TRUE, 
-                                           bw = TRUE))
-     
-     g5 <- 
-     ggplot(data, 
-            aes(x=federal,
-                y=words)) + 
-       
-       geom_violin(alpha = 0.8, 
-                   fill = 'black') +
-       geom_jitter(width = 0.3, 
-                   alpha = 0.1,
-                   color = 'black',
-                   size = 1.5,
-                   stroke = 1,
-                   pch = 1) +
-       labs(x = '',
-            y = 'Words',
-            title = 'Version 5') + 
-       theme_sfi() +
-       scale_fill_manual(name = '',
-                         values = make_colors(length(unique(data$federal)),
-                                              categorical = TRUE, 
-                                              bw = TRUE))
-     
-     # violin plot
-     g6 <- 
-     ggplot(data, 
-            aes(x=federal,
-                y=words)) + 
-       geom_violin(alpha = 0.5, 
-                   fill = 'grey') +
-       geom_jitter(width = 0.5, 
-                   alpha = 0.1,
-                   color = 'black',
-                   size = 1,
-                   pch = 2) +
-       labs(x = '',
-            y = 'Words',
-            title = 'Version 6') + 
-       theme_sfi() +
-       scale_fill_manual(name = '',
-                         values = make_colors(length(unique(data$federal)),
-                                              categorical = TRUE, 
-                                              bw = TRUE))
+  ggplot(data, 
+        aes(x=federal,
+            y=words)) + 
    
+   geom_violin(alpha = 0.8, 
+               fill = 'black') +
+   geom_jitter(width = 0.3, 
+               alpha = 0.1,
+               color = 'black',
+               size = 1.5,
+               stroke = 1,
+               pch = 1) +
+   labs(x = '',
+        y = 'Words',
+        title = 'Version 1') + 
+   theme_sfi() +
+   scale_fill_manual(name = '',
+                     values = make_colors(length(unique(data$federal)),
+                                          categorical = TRUE, 
+                                          bw = TRUE))
   
-     # density with line type for groups
-     g7 <- 
-     ggplot(data, aes(x = words)) +
-       stat_density(aes(group = federal, linetype = federal),
-                    position="identity",
-                    geom="line", kernel = "gaussian", size = 0.5) +
-       labs(x = 'Words', 
-            y = 'Density', 
-            title = 'Version 7 (similar to authro') + 
-       theme_sfi() + scale_linetype_discrete(name = '')
-     
+
   
   
   
