395f1c56afab733529f02dbe5f592f969f61d7ed
Ben Brew <benmbrew@gmail.com>
2018-08-18 14:31:03 -0400

    implement text inside waffle plot

--- R/sfi_plot_alexander_4.R
+++ R/sfi_plot_alexander_4.R
@@ -34,130 +34,205 @@ sfi_plot_alexander_4 <- function(){
   
   # create names vectors for values and colors 
   values <- data$value[data$group == 'Claim type']
-  names(values) <- paste0(data$key[data$group == 'Claim type'], ' (',values,'%)')
+  values_with_per <- values
+  names(values_with_per) <- paste0(data$key[data$group == 'Claim type'], ' (',values,'%)')
+  names(values) <- data$key[data$group == 'Claim type']
+  values_with_per <- sort(values_with_per, decreasing = TRUE)
   values <- sort(values, decreasing = TRUE)
   colors <- make_colors(length(values), bw = TRUE)
   
-  # do the same for the national origin filter
+  # create names vectors for values and colors 
   values_2 <- data$value[data$group == 'National origin']
-  names(values_2) <- paste0(data$key[data$group == 'National origin'], ' (',values_2,'%)')
+  values_2_with_per <- values_2
+  names(values_2_with_per) <- paste0(data$key[data$group == 'National origin'], ' (',values_2,'%)')
+  names(values_2) <- data$key[data$group == 'National origin']
+  values_2_with_per <- sort(values_2_with_per, decreasing = TRUE)
   values_2 <- sort(values_2, decreasing = TRUE)
   colors_2 <- make_colors(length(values_2), bw = TRUE)
   
+  
   # plot - Each plot has two version 1: the first for Claim type the second for national origin
   g1 <-
+    waffle(parts = values_with_per, 
+           title = 'Version 1 (Claim type)',
+           colors = colors, 
+           rows = 10,
+           size  = 0.1,
+           flip = FALSE) + 
+    theme(legend.key.height = 
+            unit(1, 'mm')) +
+    annotate(geom = 'text',
+             label = 'Retaliation',
+             cex = 7,
+             color = 'white',
+             x = 2.5,
+             y = 4) +
+    annotate(geom = 'text',
+             label = 'Sex',
+             cex = 7,
+             color = 'white',
+             x = 5.5,
+             y = 5) +
+    annotate(geom = 'text',
+             label = 'Race',
+             cex = 7,
+             color = 'white',
+             x = 8,
+             y = 6) +
+    annotate(geom = 'text',
+             label = 'Exemption',
+             angle = 90,
+             cex = 5,
+             color = 'white',
+             x = 10,
+             y = 2) +
+    annotate(geom = 'text',
+             label = 'Religion',
+             angle = 90,
+             cex = 4,
+             color = 'white',
+             x = 10,
+             y = 9.3) +
+    annotate(geom = 'text',
+             label = paste0('National', ' origin'),
+             angle = 90,
+             cex = 4.5,
+             color = 'white',
+             x = 10,
+             y = 6) 
+  
+    # plot - Each plot has two version 1: the first for Claim type the second for national origin
+  g2 <-
     waffle(parts = values, 
                title = 'Version 1 (Claim type)',
                colors = colors, 
-               rows = 3,
-               size  = 1,
-               flip = FALSE) +
-    theme_sfi(lp = c(0.5,-1))
+               rows = 8,
+               size  = 0.1,
+               flip = FALSE) + 
+  theme(legend.key.height = 
+          unit(1, 'mm')) +
+    annotate(geom = 'text',
+             label = '38%',
+             cex = 10,
+             color = 'white',
+             x = 2.5,
+             y = 4) +
+    annotate(geom = 'text',
+             label = '26%',
+             cex = 10,
+             color = 'white',
+             x = 7,
+             y = 5) +
+    annotate(geom = 'text',
+             label = '24%',
+             cex = 10,
+             color = 'white',
+             x = 10,
+             y = 5) +
+    annotate(geom = 'text',
+             label = '5%',
+             cex = 8,
+             color = 'white',
+             x = 12,
+             y = 3) +
+    annotate(geom = 'text',
+             label = '5%',
+             cex = 8,
+             color = 'white',
+             x = 12,
+             y = 7) +
+    annotate(geom = 'text',
+             label = '2%',
+             cex = 8,
+             color = 'white',
+             x = 13,
+             y = 4) 
+    
   
-  g1
-  
-  
-  g2 <-
-    waffle(parts = values_2, 
-               title = 'Version 1 (National origin)',
-               colors = colors_2, 
-               rows = 10,
-               size  = 1,
-               flip = FALSE)
-  
-  # small size
+  # plot - Each plot has two version 1: the first for Claim type the second for national origin
   g3 <-
-    waffle(parts = values,
-               title = 'Version 2 (Claim type)',
-               colors = colors, 
-               rows = 10,
-               size  = 0.1,
-               flip = FALSE)
+    waffle(parts = values_2_with_per, 
+           title = 'Version 1 (National origin)',
+           colors = colors_2, 
+           rows = 10,
+           size  = 0.1,
+           flip = FALSE) + 
+    theme(legend.key.height = unit(1, 'mm')) +
+    annotate(geom = 'text',
+             label = 'African American',
+             cex = 7,
+             color = 'white',
+             x = 4,
+             y = 5) +
+    annotate(geom = 'text',
+             label = 'Hispanic/Latino',
+             angle = 90,
+             cex = 7,
+             color = 'white',
+             x = 8,
+             y = 5) +
+    annotate(geom = 'text',
+             label = 'Asian American',
+             angle = 90,
+             cex = 3,
+             color = 'white',
+             x = 9,
+             y = 9) +
+    annotate(geom = 'text',
+             label = 'Indian',
+             angle = 90,
+             cex = 5,
+             color = 'white',
+             x = 10,
+             y = 7) +
+    annotate(geom = 'text',
+             label = paste0('Middle', '\n', 'Eastern'),
+             angle = 90,
+             cex = 2.5,
+             color = 'white',
+             x = 10,
+             y = 10) 
   
-  # small size
+  # plot - Each plot has two version 1: the first for Claim type the second for national origin
   g4 <-
-    
-    waffle(parts = values_2,
-               title = 'Version 2 (National origin)',
-               colors = colors_2, 
-               rows = 10,
-               size  = 0.1,
-               flip = FALSE)
+    waffle(parts = values_2, 
+           title = 'Version 1 (National origin)',
+           colors = colors_2, 
+           rows = 10,
+           size  = 0.1,
+           flip = FALSE) + 
+    theme(legend.key.height = unit(1, 'mm')) +
+    annotate(geom = 'text',
+             label = '71%',
+             cex = 7,
+             color = 'white',
+             x = 6,
+             y = 4) +
+    annotate(geom = 'text',
+             label = '15%',
+             cex = 7,
+             color = 'white',
+             x = 8.5,
+             y = 4) +
+    annotate(geom = 'text',
+             label = '8%',
+             cex = 5,
+             color = 'white',
+             x = 9,
+             y = 8) +
+    annotate(geom = 'text',
+             label = '5%',
+             cex = 5,
+             color = 'white',
+             x = 10,
+             y = 7) +
+    annotate(geom = 'text',
+             label = '1%',
+             cex = 4,
+             color = 'white',
+             x = 10,
+             y = 10) 
   
-  
-  
-  # Floating bubbles diagnol for national origin
-  
-  gd <- data %>% filter(group == 'National origin') %>%
-    arrange(value) %>%
-    mutate(nudgey = seq(0.5, 2, length = 6)) %>%
-    mutate(x = seq(0, 6, length = 6),
-           y = c(1, 2, 3, 4, 5, 6))
-  g5 <-ggplot(data = gd, 
-               aes(x = x, y = y)) +
-    xlim(c(-1,7)) + ylim(c(0,8)) +
-    geom_point(aes(size = value), 
-               alpha = 0.4,
-               color ='black') + 
-    geom_text(aes(y = y + nudgey,
-                  label = paste0(key, ' ', '\n', value,'%')),
-              alpha = 0.7,
-              size = 3,
-              color = 'black')+
-    labs(x = '',
-         y = '',
-         title = 'Version 3 (National origin)') + 
-    scale_size_area(name = '',
-                          max_size = 40) +
-    theme(legend.position = 'none',
-      axis.text.x = element_blank(),
-      axis.text.y = element_blank(),
-      axis.ticks = element_blank()) + 
-    theme_sfi(gM = FALSE,
-              lp = 'none',
-              gm = FALSE)
-  
-
-    
-  # g13 THE REST ARE THE SIDE BAR CHARTS THAT THEY KINDA LIKED
-    g6 <-
-  ggplot(data = data %>% filter(group == 'Claim type'),
-           aes(reorder(x = key, value),
-               y = value)) +
-    geom_bar(stat = 'identity',
-             alpha = 0.8) +
-    coord_flip() +
-    theme_sfi() +
-    labs(x = 'Claim type',
-         y = 'Percent',
-         title = 'Version 4 (Claim type)') +
-    geom_text(aes(label = paste0(round(value, digits = 1), '%')),
-              nudge_y = 3,
-              size = 4,
-              alpha = 0.7) +
-    theme(axis.text.x = element_text(hjust = 0.5)) +
-    ylim(0, 55)
-
-  #g14 with percentage labels
- g7 <-
-   ggplot(data = data %>% filter(group == 'National origin'),
-         aes(reorder(x = key, value),
-             y = value)) +
-    geom_bar(stat = 'identity',
-             alpha = 0.8) +
-    coord_flip() +
-    theme_sfi() +
-    labs(x = 'Claim type',
-         y = 'Percent',
-         title = 'Version 4 (National origin)') +
-    geom_text(aes(label = paste0(round(value, digits = 1), '%')),
-              nudge_y = 5.5,
-              size = 4,
-              alpha = 0.7) +
-    theme(axis.text.x = element_text(hjust = 0.5)) +
-    ylim(0, 100)
-
  
- 
-  return(list(g1, g2, g3, g4, g5, g6, g7))
+  return(list(g1, g2, g3, g4))
 }
