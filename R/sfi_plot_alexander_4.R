22500a56a2bc70f51cae436fafcb976d80f684ca
Ben Brew <benmbrew@gmail.com>
2018-09-12 17:35:12 -0400

    finished alexander plots

--- R/sfi_plot_alexander_4.R
+++ R/sfi_plot_alexander_4.R
@@ -5,8 +5,8 @@
 #' @return a list of plots
 #' @import ggplot2
 #' @import dplyr
-#' @import waffle 
-#' @import plotrix
+#' @import plotly
+
 
 sfi_plot_alexander_4 <- function(){
   
@@ -28,211 +28,102 @@ sfi_plot_alexander_4 <- function(){
   
   # Get percentage
   data$value <- data$value * 100
-
-  # ---------------------------------------------
-  # waffle chart
   
-  # create names vectors for values and colors 
-  values <- data$value[data$group == 'Claim type']
-  values_with_per <- values
-  names(values_with_per) <- paste0(data$key[data$group == 'Claim type'], ' (',values,'%)')
-  names(values) <- data$key[data$group == 'Claim type']
-  values_with_per <- sort(values_with_per, decreasing = TRUE)
-  values <- sort(values, decreasing = TRUE)
-  colors <- make_colors(length(values), bw = TRUE)
+  # subset to claim type 
+  dat_claim <- data[data$group == 'Claim type',]
   
-  # create names vectors for values and colors 
-  values_2 <- data$value[data$group == 'National origin']
-  values_2_with_per <- values_2
-  names(values_2_with_per) <- paste0(data$key[data$group == 'National origin'], ' (',values_2,'%)')
-  names(values_2) <- data$key[data$group == 'National origin']
-  values_2_with_per <- sort(values_2_with_per, decreasing = TRUE)
-  values_2 <- sort(values_2, decreasing = TRUE)
-  colors_2 <- make_colors(length(values_2), bw = TRUE)
+  # subset to National origin
+  dat_origin <- data[data$group == 'National origin',]
+  
+  # get a color vector
+  colors_claim  <- make_colors(length(unique(dat_claim$key)), bw = TRUE)
+  colors_origin  <- make_colors(length(unique(dat_origin$key)), bw = TRUE)
+  
+  # sort data
+  dat_claim <- dat_claim[order(dat_claim$value, decreasing = TRUE),]
+  dat_origin <- dat_origin[order(dat_origin$value, decreasing = TRUE),]
   
   
-  # plot - Each plot has two version 1: the first for Claim type the second for national origin
-  g1 <-
-    waffle(parts = values_with_per, 
-           title = 'Version 1 (Claim type)',
-           colors = colors, 
-           rows = 10,
-           size  = 0.1,
-           flip = FALSE) + 
-    theme(legend.key.height = 
-            unit(1, 'mm')) +
-    annotate(geom = 'text',
-             label = 'Retaliation',
-             cex = 7,
-             color = 'white',
-             x = 2.5,
-             y = 4) +
-    annotate(geom = 'text',
-             label = 'Sex',
-             cex = 7,
-             color = 'white',
-             x = 5.5,
-             y = 5) +
-    annotate(geom = 'text',
-             label = 'Race',
-             cex = 7,
-             color = 'white',
-             x = 8,
-             y = 6) +
-    annotate(geom = 'text',
-             label = 'Exemption',
-             angle = 90,
-             cex = 5,
-             color = 'white',
-             x = 10,
-             y = 2) +
-    annotate(geom = 'text',
-             label = 'Religion',
-             angle = 90,
-             cex = 4,
-             color = 'white',
-             x = 10,
-             y = 9.3) +
-    annotate(geom = 'text',
-             label = paste0('National', ' origin'),
-             angle = 90,
-             cex = 4.5,
-             color = 'white',
-             x = 10,
-             y = 6) 
+  ##------- USING PLOTLY
+  # remove dark color
+  # get font object
+  inside_f <- list(
+    family = "Computer modern",
+    size = 12,
+    color = 'white'
+  )
   
-    # plot - Each plot has two version 1: the first for Claim type the second for national origin
-  g2 <-
-    waffle(parts = values, 
-               title = 'Version 1 (Claim type)',
-               colors = colors, 
-               rows = 8,
-               size  = 0.1,
-               flip = FALSE) + 
-  theme(legend.key.height = 
-          unit(1, 'mm')) +
-    annotate(geom = 'text',
-             label = '38%',
-             cex = 10,
-             color = 'white',
-             x = 2.5,
-             y = 4) +
-    annotate(geom = 'text',
-             label = '26%',
-             cex = 10,
-             color = 'white',
-             x = 7,
-             y = 5) +
-    annotate(geom = 'text',
-             label = '24%',
-             cex = 10,
-             color = 'white',
-             x = 10,
-             y = 5) +
-    annotate(geom = 'text',
-             label = '5%',
-             cex = 8,
-             color = 'white',
-             x = 12,
-             y = 3) +
-    annotate(geom = 'text',
-             label = '5%',
-             cex = 8,
-             color = 'white',
-             x = 12,
-             y = 7) +
-    annotate(geom = 'text',
-             label = '2%',
-             cex = 8,
-             color = 'white',
-             x = 13,
-             y = 4) 
+  outside_f_claim <- list(
+    family = "Computer modern",
+    size = 12,
+    color = colors_claim 
+  )
+  
+  outside_f_origin <- list(
+    family = "Computer modern",
+    size = 12,
+    color = colors_origin
+  )
+  
+  # text font 
+  t <- list(
+    family = "Computer modern",
+    size = 10,
+    color = '#353535'
+  )
+  
+  
+  # plot
+  g1 <- plot_ly(dat_claim,
+          labels = ~key, 
+          values = ~round(value),
+          type ='pie',
+          hole = 0.5,
+          textposition = 'outside',
+          textinfo = 'percent+label',
+          rotation = 90,
+          outsidetextfont = outside_f_claim,
+          marker = list(colors = colors_claim),
+          width = 450, 
+          height = 300)  %>%
+    config(displayModeBar = F) %>%
     
+    layout(title ='' ,
+           font = t,
+           showlegend = F,
+           annotations = list(
+             showarrow = FALSE,
+             text = paste0('Figure 4a', '\n', 'Title VII Claim types', '\n', 'from complaint text'),
+             font = t), 
+           xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
+           yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE))
   
-  # plot - Each plot has two version 1: the first for Claim type the second for national origin
-  g3 <-
-    waffle(parts = values_2_with_per, 
-           title = 'Version 1 (National origin)',
-           colors = colors_2, 
-           rows = 10,
-           size  = 0.1,
-           flip = FALSE) + 
-    theme(legend.key.height = unit(1, 'mm')) +
-    annotate(geom = 'text',
-             label = 'African American',
-             cex = 7,
-             color = 'white',
-             x = 4,
-             y = 5) +
-    annotate(geom = 'text',
-             label = 'Hispanic/Latino',
-             angle = 90,
-             cex = 7,
-             color = 'white',
-             x = 8,
-             y = 5) +
-    annotate(geom = 'text',
-             label = 'Asian American',
-             angle = 90,
-             cex = 3,
-             color = 'white',
-             x = 9,
-             y = 9) +
-    annotate(geom = 'text',
-             label = 'Indian',
-             angle = 90,
-             cex = 5,
-             color = 'white',
-             x = 10,
-             y = 7) +
-    annotate(geom = 'text',
-             label = paste0('Middle', '\n', 'Eastern'),
-             angle = 90,
-             cex = 2.5,
-             color = 'white',
-             x = 10,
-             y = 10) 
   
-  # plot - Each plot has two version 1: the first for Claim type the second for national origin
-  g4 <-
-    waffle(parts = values_2, 
-           title = 'Version 1 (National origin)',
-           colors = colors_2, 
-           rows = 10,
-           size  = 0.1,
-           flip = FALSE) + 
-    theme(legend.key.height = unit(1, 'mm')) +
-    annotate(geom = 'text',
-             label = '71%',
-             cex = 7,
-             color = 'white',
-             x = 6,
-             y = 4) +
-    annotate(geom = 'text',
-             label = '15%',
-             cex = 7,
-             color = 'white',
-             x = 8.5,
-             y = 4) +
-    annotate(geom = 'text',
-             label = '8%',
-             cex = 5,
-             color = 'white',
-             x = 9,
-             y = 8) +
-    annotate(geom = 'text',
-             label = '5%',
-             cex = 5,
-             color = 'white',
-             x = 10,
-             y = 7) +
-    annotate(geom = 'text',
-             label = '1%',
-             cex = 4,
-             color = 'white',
-             x = 10,
-             y = 10) 
+  # plot
+  g2 <- plot_ly(dat_origin,
+                labels = ~key, 
+                values = ~round(value),
+                type ='pie',
+                hole = 0.5,
+                textposition = 'outside',
+                textinfo = 'percent+label',
+                rotation = 10,
+                outsidetextfont = outside_f_origin,
+                marker = list(colors = colors_origin),
+                width = 450, 
+                height =300)  %>%
+    config(displayModeBar = F) %>%
+    
+    layout(title ='' ,
+           font = t,
+           showlegend = F,
+           annotations = list(
+             showarrow = FALSE,
+             text = paste0('Figure 4b', '\n', "Title VII Plaintiff's race", '\n', 'from complaint text'),
+             font = t), 
+           xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
+           yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE))
   
  
-  return(list(g1, g2, g3, g4))
+  return(list(g1,g2))
 }
--- R/sfi_plot_alexander_7.R
+++ R/sfi_plot_alexander_7.R
@@ -5,7 +5,7 @@
 #' @return a list of plots
 #' @import ggplot2
 #' @import dplyr
-#' @import waffle
+#' @import plotly
 
 sfi_plot_alexander_7 <- function(){
 #   
@@ -13,107 +13,63 @@ sfi_plot_alexander_7 <- function(){
   data <- all_data$alexander$f7
   data$value <- data$value * 100
   
-  # create names vectors for values and colors 
-  values <- data$value
-  values_with_per <- data$value
-  names(values_with_per) <- paste0(data$key, ' (',values,'%)')
-  names(values) <- paste0(data$key)
-  values_with_per <- sort(values_with_per, decreasing = TRUE)
-  values <- sort(values, decreasing = TRUE)
-  colors <- make_colors(length(values), bw = TRUE)
+  # get colors
+  colors  <- make_colors(length(unique(data$key)), bw = TRUE)
+
+  # sort data
+  data <- data[order(data$value, decreasing = TRUE),]
+
+  # get font object
+  inside_f <- list(
+    family = "Computer modern",
+    size = 12,
+    color = 'white'
+  )
   
-  # ---------------------------------------------
-  # waffle chart
-  # plot - Each plot has two version 1: the first for Claim type the second for national origin
-  g1 <-
-    waffle(parts = values_with_per, 
-           title = 'Version 1',
-           colors = colors, 
-           rows = 10,
-           size  = 0.1,
-           flip = FALSE) + 
-    theme(legend.key.height = 
-            unit(1, 'mm')) +
-    annotate(geom = 'text',
-             label = 'Adopted',
-             cex = 7,
-             color = 'white',
-             x = 5,
-             y = 5) +
-    annotate(geom = 'text',
-             label = 'Partial',
-             angle = 90,
-             cex = 7,
-             color = 'white',
-             x = 9,
-             y = 7) +
-    annotate(geom = 'text',
-             label = 'Unknown',
-             angle = 90,
-             cex = 7,
-             color = 'white',
-             x = 10,
-             y = 5) +
-    annotate(geom = 'text',
-             label = 'Rejected',
-             cex = 2,
-             color = 'white',
-             x = 10,
-             y = 9) +
-    annotate(geom = 'text',
-             label = 'Other',
-             cex = 3,
-             color = '#444444',
-             x = 10,
-             y = 10) 
+  outside_f <- list(
+    family = "Computer modern",
+    size = 12,
+    color = colors
+  )
   
   
+  # text font 
+  t <- list(
+    family = "Computer modern",
+    size = 12,
+    color = '#353535'
+  )
   
   
-  g2 <-
-    waffle(parts = values, 
-           title = 'Version 2',
-           colors = colors, 
-           rows = 10,
-           size  = 0.1,
-           flip = FALSE) + 
-    theme(legend.key.height = 
-            unit(1, 'mm')) +
-    annotate(geom = 'text',
-             label = '85%',
-             cex = 10,
-             color = 'white',
-             x = 5,
-             y = 5) +
-    annotate(geom = 'text',
-             label = '7%',
-             cex = 6,
-             color = 'white',
-             x = 9,
-             y = 7) +
-    annotate(geom = 'text',
-             label = '6%',
-             cex = 6,
-             color = 'white',
-             x = 10,
-             y = 7) +
-    annotate(geom = 'text',
-             label = '1%',
-             cex = 5,
-             color = 'white',
-             x = 10,
-             y = 9) +
-    annotate(geom = 'text',
-             label = '1%',
-             cex = 5,
-             color = '#444444',
-             x = 10,
-             y = 10) 
+  # plot
+  g1 <- plot_ly(data,
+                labels = ~key, 
+                values = ~round(value),
+                type ='pie',
+                hole = 0.6,
+                textposition = 'outside',
+                textinfo = 'percent+label',
+                rotation = 120,
+                outsidetextfont = outside_f,
+                marker = list(colors = colors),
+                width = 550, 
+                height = 300)  %>%
+    config(displayModeBar = F) %>%
+    
+    layout(title ='' ,
+           font = t,
+           showlegend = F,
+           annotations = list(
+             showarrow = FALSE,
+             text = paste0('Figure 7', '\n', "District courst judges'" , '\n', 'actions on magistrate', '\n','judges'),
+             font = t), 
+           xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
+           yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE))
+  
+  g1
   
   
-  
-  
-  return(list(g1, g2))
+  return(g1)
   
 }
   
