20212f14879e0d1ef396cb6bbb1cd603663629d6
Ben Brew <benmbrew@gmail.com>
2018-09-17 17:07:26 -0400

    finished feldman

--- R/sfi_plot_feldman_2.R
+++ R/sfi_plot_feldman_2.R
@@ -19,135 +19,82 @@ sfi_plot_feldman_2 <- function(){
   data$federal <- ifelse(data$federal == '1', 'Federal', 'State')
 
   
-  # VERSION 1
-  g1 <- 
-    ggplot(data, 
-           aes(x=federal,
-               y=words)) + 
-    
-    geom_violin(alpha = 0.8, 
-                fill = 'black') +
-    geom_jitter(width = 0.3, 
-                alpha = 0.1,
-                color = 'black',
-                size = 1.5,
-                stroke = 1,
-                pch = 1) +
+  #Version 1
+  g1<- ggplot(data, 
+              aes(x = federal, 
+                  y = words)) +
+    stat_ydensity(geom="segment", 
+                  adjust = 20,
+                  scale = 'area',
+                  aes(xend=..x..+..scaled../3.5, 
+                      yend=..y.., 
+                      alpha=(..scaled../3)^5), 
+                  size=3, 
+                  color = 'darkgrey',
+                  trim=TRUE) +
+    stat_ydensity(geom="segment", 
+                  adjust = 20,
+                  scale = 'area',
+                  aes(xend=..x..-..scaled../3.5, 
+                      yend=..y.., 
+                      alpha=(..scaled../3)^5), 
+                  size=3, 
+                  linetype = 1,
+                  color = 'darkgrey',
+                  
+                  trim=TRUE) +
     labs(x = '',
-         y = 'Words',
-         title = 'Version 1',
-         caption = paste0('The violin plot is a mirrored density plot showing', '\n', 
-                          'a compact display of a continuous distribution')) + 
-    theme_sfi() +
-    scale_fill_manual(name = '',
-                      values = make_colors(length(unique(data$federal)),
-                                           categorical = TRUE, 
-                                           bw = TRUE))
-  
-  
-  # VERSION 2
-  g2 <- 
-    ggplot(data, 
-           aes(x=federal,
-               y=words)) + 
-    
-    geom_violin(alpha = 0.8, 
-                fill = 'black') +
-    geom_jitter(width = 0.3, 
-                alpha = 0.3,
+         y = 'Clarity Score',
+         title = 'Figure 2. Distribution of Federal and State Court Opinion Word Length (Version 1)',
+         caption = 'The shading is a function of the point distribution') +
+    geom_jitter(size = 1,
                 color = 'black',
-                size = 0.5,
-                stroke = 0.5,
-                pch = 1) +
-    labs(x = '',
-         y = 'Words',
-         title = 'Version 2',
-         caption = paste0('The violin plot is a mirrored density plot showing', '\n', 
-                          'a compact display of a continuous distribution')) + 
-    theme_sfi() +
-    scale_fill_manual(name = '',
-                      values = make_colors(length(unique(data$federal)),
-                                           categorical = TRUE, 
-                                           bw = TRUE))
-  
-  
-  # VERSION 3
-  g3 <- 
-    ggplot(data, 
-           aes(x=federal,
-               y=words)) + 
-    
-    geom_violin(alpha = 0.8, 
-                fill = 'black') +
-    geom_jitter(width = 0.3, 
-                alpha = 0.2,
-                color = 'grey',
-                size = 0.7,
-                stroke = 1,
+                width = 0.3,
+                alpha = 0.5,
                 pch = 16) +
+    theme_sfi(lp = 'none',
+              y_axis_title_style = 'bold',
+              x_axis_title_style = 'bold',
+              title_style = 'bold')
+  
+  # Version 2
+  g2 <- ggplot(data,
+               aes(x = federal,
+                   y = words)) +
+    stat_ydensity(geom="segment",
+                  adjust = 20,
+                  scale = 'area',
+                  aes(xend=..x..+..scaled../3,
+                      yend=..y..,
+                      alpha=..scaled../2),
+                  size=3,
+                  color = 'darkgrey',
+                  trim=FALSE) +
+    stat_ydensity(geom="segment",
+                  adjust = 20,
+                  scale = 'area',
+                  aes(xend=..x..-..scaled../3,
+                      yend=..y..,
+                      alpha=..scaled../2),
+                  size=3,
+                  linetype = 1,
+                  color = 'darkgrey',
+                  trim=FALSE) +
     labs(x = '',
-         y = 'Words',
-         title = 'Version 3',
-         caption = paste0('The violin plot is a mirrored density plot showing', '\n', 
-                          'a compact display of a continuous distribution')) + 
-    theme_sfi() +
-    scale_fill_manual(name = '',
-                      values = make_colors(length(unique(data$federal)),
-                                           categorical = TRUE, 
-                                           bw = TRUE))
-  
-  
-  # VERSION 4
-  g4 <- 
-    ggplot(data, 
-           aes(x=federal,
-               y=words)) + 
-    
-    geom_violin(alpha = 0.8, 
-                fill = 'black') +
-    geom_jitter(width = 0.2, 
-                alpha = 0.1,
-                color = 'lightgrey',
-                size = 0.5,
-                stroke = 0.5,
-                pch = 19) +
-    labs(x = '',
-         y = 'Words',
-         title = 'Version 4',
-         caption = paste0('The violin plot is a mirrored density plot showing', '\n', 
-                          'a compact display of a continuous distribution')) + 
-    theme_sfi() +
-    scale_fill_manual(name = '',
-                      values = make_colors(length(unique(data$federal)),
-                                           categorical = TRUE, 
-                                           bw = TRUE))
-  
-  # VERSION 5
-  g5 <- 
-    ggplot(data, 
-           aes(x=federal,
-               y=words)) + 
-    geom_violin(alpha = 0.8, 
-                fill = 'grey') +
-    geom_jitter(width = 0.3, 
-                alpha = 0.1,
+         y = 'Clarity Score',
+         title = 'Figure 2. Distribution of Federal and State Court Opinion Word Length (Version 2)',
+         caption = 'The shading is a function of the point distribution') +
+    geom_jitter(size = 1,
                 color = 'black',
-                size = 0.7,
-                stroke = 1,
+                width = 0.3,
+                alpha = 0.8,
                 pch = 16) +
-    labs(x = '',
-         y = 'Words',
-         title = 'Version 5',
-         caption = paste0('The violin plot is a mirrored density plot showing', '\n', 
-                          'a compact display of a continuous distribution')) + 
-    theme_sfi() +
-    scale_fill_manual(name = '',
-                      values = make_colors(length(unique(data$federal)),
-                                           categorical = TRUE, 
-                                           bw = TRUE))
-
+    theme_sfi(lp = 'none',
+              y_axis_title_style = 'bold',
+              x_axis_title_style = 'bold',
+              title_style = 'bold') 
   
-  out <- list(g1, g2, g3, g4, g5)
+  out <- list(g1, g2)
   
   return(out)
   
